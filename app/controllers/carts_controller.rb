ec73b0717241ac0edc45688064dc174a07a9dd31
jkxruby <jkxruby@gmail.com>
2018-01-01 18:48:09 +0800

    再次隐藏一些代码

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,11 @@
-class CartsController < ApplicationController
-  def clean
-    current_cart.clean!
-    flash[:warning] = "Cart emptied!"
-    redirect_to products_path
-  end
-
-  def checkout
-    @order = Order.new
-  end
-end
+# class CartsController < ApplicationController
+#   def clean
+#     current_cart.clean!
+#     flash[:warning] = "Cart emptied!"
+#     redirect_to products_path
+#   end
+#
+#   def checkout
+#     @order = Order.new
+#   end
+# end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,24 +1,24 @@
-class FavoritesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_product
-
-  def create
-      if Favorite.create(product: @product, user: current_user)
-          redirect_to :back, notice: "Product #{@product.title} has been favorited"
-      else
-          redirect_to :back, notice: "Something went wrong...favorite failed"
-      end
-  end
-
-  def destroy
-      @favorite = Favorite.find_by(user: current_user,product: @product)
-      @favorite.destroy
-      redirect_to :back, alert: "Removed product #{@product.title} from favorite"
-  end
-
-  private
-
-  def set_product
-      @product = Product.find(params[:product_id] || params[:id])
-  end
-end
+# class FavoritesController < ApplicationController
+#   before_action :authenticate_user!
+#   before_action :set_product
+#
+#   def create
+#       if Favorite.create(product: @product, user: current_user)
+#           redirect_to :back, notice: "Product #{@product.title} has been favorited"
+#       else
+#           redirect_to :back, notice: "Something went wrong...favorite failed"
+#       end
+#   end
+#
+#   def destroy
+#       @favorite = Favorite.find_by(user: current_user,product: @product)
+#       @favorite.destroy
+#       redirect_to :back, alert: "Removed product #{@product.title} from favorite"
+#   end
+#
+#   private
+#
+#   def set_product
+#       @product = Product.find(params[:product_id] || params[:id])
+#   end
+# end
